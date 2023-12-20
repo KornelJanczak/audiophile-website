ac6296ece8501061641e203f26be15ce234ac5db
Kornel Janczak <korneljanczak10@gmail.com>
2023-12-20 11:47:12 +0100

    Create Responsive Image component and query for hero section

--- components/Home/HeroSection/HeroSection.tsx
+++ components/Home/HeroSection/HeroSection.tsx
@@ -1,35 +1,46 @@
 "use client";
 import React from "react";
 import classes from "./HeroSection.module.css";
-import heroImg from "../../../public/assets/home/desktop/image-hero.jpg";
-import Image from "next/image";
 import Button from "../../UI/Button/Button";
+import ResponsiveImage from "@/components/UI/ResponsiveImage";
+import { useRouter } from "next/navigation";
 
 const HeroSection: React.FC = () => {
+  const router = useRouter();
   return (
-    <section
-      className={classes.hero__section}
-      style={{ backgroundImage: `${heroImg}` }}
-    >
-      <div className={classes.hero}>
-        <div className={classes.hero__text__box}>
-          <p className={classes.hero__span}>NEW PRODUCT</p>
-          <h1 className={classes.hero__h1}>XX99 MARK II HEADPHONES</h1>
-          <p className={classes.hero__text}>
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
-          </p>
-          <Button style={classes.btn__hero}>SEE PRODUCT</Button>
+    <section className={classes.hero__section}>
+      <div className={classes.container}>
+        <div className={classes.hero}>
+          <div className={classes.hero__text__box}>
+            <p className={classes.hero__span}>NEW PRODUCT</p>
+            <h1 className={classes.hero__h1}>XX99 MARK II HEADPHONES</h1>
+            <p className={classes.hero__text}>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <Button
+              style={classes.btn__hero}
+              onClick={() =>
+                router.push("/categories/product/xx99-mark-two-headphones")
+              }
+            >
+              SEE PRODUCT
+            </Button>
+          </div>
+          <div className={classes.hero__img__box}></div>
         </div>
-        <div className={classes.hero__img__box}>
-          {/* <Image
-            src={heroImg}
-            alt="hero-img"
-            height={0}
-            width={0}
-            className={classes.hero__img}
-          /> */}
-        </div>
+        <ResponsiveImage
+          images={{
+            desktop: "/assets/home/desktop/Bitmap.png",
+            tablet: "/assets/home/tablet/Bitmap.png",
+            mobile: "/assets/home/mobile/Bitmap.png",
+          }}
+          altText="Hero Logo"
+          width={546}
+          height={598}
+          pictureClassName={classes.hero__logo}
+          imgClassName={classes.hero__logo}
+        />
       </div>
     </section>
   );
