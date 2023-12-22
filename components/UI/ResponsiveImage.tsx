98abb6a4227d35d9faf10323e06693dd3e4551a6
Kornel Janczak <korneljanczak10@gmail.com>
2023-12-22 23:14:03 +0100

    style: media query for Hero and Showcase section

--- components/UI/ResponsiveImage.tsx
+++ components/UI/ResponsiveImage.tsx
@@ -23,7 +23,7 @@ const ResponsiveImage = ({
 }: Data) => {
   return (
     <picture className={pictureClassName}>
-      <source media="(min-width: 769px)" srcSet={images.desktop} />
+      <source media="(min-width: 800px)" srcSet={images.desktop} />
       <source media="(min-width: 480px)" srcSet={images.tablet} />
       <source media="(min-width: 1px)" srcSet={images.mobile} />
       <Image
