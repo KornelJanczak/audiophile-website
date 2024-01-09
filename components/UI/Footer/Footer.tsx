f8593a973bdd23582cc8536e1f75e166b03c9bf6
Kornel Janczak <korneljanczak10@gmail.com>
2024-01-09 20:29:52 +0100

    feat: animation for footer

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -6,6 +6,8 @@ import TwitterIcon from "@/public/Icons/TwitterIcon";
 import classes from "./Footer.module.css";
 import Link from "next/link";
 import LogoIcon from "@/public/Icons/LogoIcon";
+import { motion } from "framer-motion";
+import { scrollVariants } from "@/animations/animations";
 
 const icons = [
   { icon: <FbIcon /> },
@@ -16,7 +18,13 @@ const icons = [
 const Footer: React.FC = () => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.footer__box}>
+      <motion.div
+        className={classes.footer__box}
+        variants={scrollVariants}
+        initial={{ y: 25, opacity: 0 }}
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.3 }}
+      >
         <div className={classes.line} />
         <div className={classes.footer__content__box}>
           <LogoIcon className={classes.header__logo} />
@@ -40,7 +48,7 @@ const Footer: React.FC = () => {
             ))}
           </ul>
         </div>
-      </div>
+      </motion.div>
     </footer>
   );
 };
