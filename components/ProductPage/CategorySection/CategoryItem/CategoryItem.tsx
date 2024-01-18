c0eb83dbd170f3b44276eb14a2ef3676f2895f4b
Kornel Janczak <korneljanczak10@gmail.com>
2024-01-18 11:33:51 +0100

    fix: change type for Categoryitem component

--- components/ProductPage/CategorySection/CategoryItem/CategoryItem.tsx
+++ components/ProductPage/CategorySection/CategoryItem/CategoryItem.tsx
@@ -1,8 +1,9 @@
-import { IPropsNode } from "@/models/@type-props";
 import classes from "./CategoryItem.module.css";
 import ScrollWrapper from "@/animations/ScrollWrapper";
 
-const CategoryItem: React.FC<IPropsNode> = ({ children }) => {
+const CategoryItem: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
     <ScrollWrapper className={classes.category__item}>{children}</ScrollWrapper>
   );
